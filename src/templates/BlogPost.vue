e17e30604d33360f6e2c8ec3cc63820d6733bf64
songroger <songroger@vip.qq.com>
2019-05-06 12:07:47 +0800

    use_sass

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -30,44 +30,55 @@ export default {
   }
 </page-query>
 
-<style>
-  /* .header {
-    margin-bottom: 70px;
-  } */
-  .article {
+<style lang="scss">
+.article {
     margin-top: 15px;
-  }
-  .article h1 {
-    font-size: 40px;
-  }
-  .article img {
-    width: 100%;
-    border-radius: 5px;
-  }
-  .article a {
-    color: #4dba87;
-    text-decoration: underline;
-  }
-  .article a:hover {
-    text-decoration: none;
-  }
-  .article span {
-    font-size: 80%;
-    margin-bottom: 20px;
-  }
-  .article ol, .article ul {
-    list-style-position: outside;
-  }
-  .article ul {
-    list-style: disc;
-    padding-left: 20px;
-  }
-  .article .content p:first-child {
-    margin-top: 15px;
-  }
-  .article .content p {
-    margin-top: 0;
-    margin-bottom: 10px;
-    line-height: 1.5;
-  }
+
+    h1 {
+        font-size: 40px;
+    }
+
+    img {
+        width: 100%;
+        border-radius: 5px;
+    }
+
+    a {
+        color: #4dba87;
+        text-decoration: underline;
+
+        &:hover {
+            text-decoration: none;
+        }
+
+    }
+
+    span {
+        font-size: 80%;
+        margin-bottom: 20px;
+    }
+
+    ol, ul {
+        list-style-position: outside;
+    }
+
+    ul {
+        list-style: disc;
+        padding-left: 20px;
+    }
+
+    .content {
+        p:first-child {
+            margin-top: 15px;
+        }
+
+        p {
+            margin-top: 0;
+            margin-bottom: 10px;
+            line-height: 1.5;
+        }
+
+    }
+
+}
 </style>
